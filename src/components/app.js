47dc81c0788e95aad277d8d395437f4e8c37c7ab
Joe Bazooka <Joe Bazooka>
2016-09-24 17:27:50 +0300

    done

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CreateTodo from './create-todo';
 import TodosList from './todos-list';
 
 const todos = [
@@ -14,12 +15,58 @@ const todos = [
 ]
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos
+    };
+  }
+
   render() {
     return (
       <div>
         <h1>React To-Do App</h1>
-        <TodosList />
+        <CreateTodo 
+          todos = { this.state.todos }
+          createTask = { this.createTask.bind(this) }
+        />
+        <TodosList 
+          todos = { this.state.todos }
+          toggleTask = { this.toggleTask.bind(this) }
+          saveTask = { this.saveTask.bind(this) }
+          deleteTask = { this.deleteTask.bind(this) }
+        />
       </div>
     );
+  }
+
+  toggleTask(task) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === task);
+    foundTodo.isCompleted = !foundTodo.isCompleted;
+    this.setState({ todos: this.state.todos });
+  }
+
+  createTask(task) {
+    this.state.todos.push({
+      task,
+      isCompleted: false
+    });
+
+    this.setState({
+      todos: this.state.todos
+    });
+  }
+
+  saveTask(oldTask, newTask) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
+    foundTodo.task = newTask;
+    this.setState({ todos: this.state.todos });
+  }
+
+  deleteTask(taskToDelete) {
+    _.remove(this.state.todos, todo => todo.task === taskToDelete);
+
+    this.setState({ todos: this.state.todos });
   }
 }
--- src/components/todos-list.js
+++ src/components/todos-list.js
@@ -1,15 +1,23 @@
+import _ from 'lodash';
 import React from 'react';
+import TodosListHeader from './todos-list-header';
+import TodosListItem from './todos-list-item';
 
 export default class TodosList extends React.Component {
+
+  renderItems() {
+    const props = _.omit(this.props, 'todos');
+
+    return _.map(this.props.todos, (todo, index) => <TodosListItem key={index} {...todo} {...props} />);
+  } 
+
   render() {
     return (
       <table>
-        <thead>
-          <tr>
-            <th>Task</th>
-            <th>Action</th>
-          </tr>
-        </thead>
+        <TodosListHeader />
+        <tbody>
+          { this.renderItems() }
+        </tbody>
       </table>
     );
   }
